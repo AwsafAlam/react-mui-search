9a63ed54b40649792a9dee38eb5c319285eff8a6
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2022-05-10 22:33:57 +0600

    sync frontend

--- src/util/config.js
+++ src/util/config.js
@@ -1,6 +1,6 @@
 const config = {
-	// BASE_API_URL: 'https://api.awsaf.mainframelabs.ltd',
-	BASE_API_URL: 'http://localhost:1111',
+	BASE_API_URL: 'https://api.awsaf.mainframelabs.ltd',
+	// BASE_API_URL: 'http://localhost:1111',
 	ENV: import.meta.env.NODE_ENV,
 	APP_URL: import.meta.env.REACT_APP_HOME
 }
