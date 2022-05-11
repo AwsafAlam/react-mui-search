6d6690bff74b88d09c078d5183d56149a0b71d95
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2022-05-11 19:10:45 +0600

    handle network error

--- src/util/config.js
+++ src/util/config.js
@@ -1,5 +1,5 @@
 const config = {
-	BASE_API_URL: 'https://api.awsaf.mainframelabs.ltd',
+	BASE_API_URL: 'https://automate.api.onlinesohopathi.com',
 	// BASE_API_URL: 'http://localhost:1111',
 	ENV: import.meta.env.NODE_ENV,
 	APP_URL: import.meta.env.REACT_APP_HOME
